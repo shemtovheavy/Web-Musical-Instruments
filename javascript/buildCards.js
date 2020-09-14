f13be2ddacdcd25da7e4ebd57f32ab93d848e3e0
theLittle10 <thelittlecitizen10@gmail.com>
2020-09-14 23:32:13 +0300

    entered menu into container

--- javascript/buildCards.js
+++ javascript/buildCards.js
@@ -1,12 +1,12 @@
 function buildCards() {
     let allInstruments = initializeInstrumentsArray();
-    var textCenter = document.getElementById("container");
+    var container = document.getElementById("container");
     var html="";
     for (var i = 0; i < allInstruments.length; i++) {
         if (i%3==0)
             html += "<div class='row pt-4'>";
         var currentInstrument = allInstruments[i];
-        html += "<div class='col-md-4'><div class='card mb-4 box-shadow' id=" + currentInstrument.name + "><div class='card-header'>";
+        html += "<div class='col-lg-4'><div class='card lg-4 box-shadow' id=" + currentInstrument.name + "><div class='card-header'>";
         html += "<h4 class='my-0 font-weight-normal'>" + currentInstrument.name + "</h4></div>";
         html += "<div class='card-body'><img src=" + currentInstrument.imagePath + " class='img-fluid' alt='Responsive image'>";
         html += "<h1><b>" + currentInstrument.price + "</b><small class='text-muted'>ILS </small></h1>";
@@ -16,7 +16,7 @@ function buildCards() {
         if (i%3==2)
             html += "</div>"
     }
-    textCenter.innerHTML = html;
+    container.innerHTML += html;
     $(".img-fluid").css({"height":200,"width":200});
 
 }
--- javascript/onCheckBoxChecked.js
+++ javascript/onCheckBoxChecked.js
@@ -9,17 +9,17 @@ function onCheckBoxChecked() {
         }
         if (item.checked && index == 1) {
             filtered = filtered.filter(function (instrument) {
-                return (instrument.price > 500 || instrument.price <= 1000);
+                return (instrument.price > 500 && instrument.price <= 1000);
             });
         }
         if (item.checked && index == 2) {
             filtered = filtered.filter(function (instrument) {
-                return (instrument.price > 1000 || instrument.price <= 1500);
+                return (instrument.price > 1000 && instrument.price <= 1500);
             });
         }
         if (item.checked && index == 3) {
             filtered = filtered.filter(function (instrument) {
-                return (instrument.price > 1500 || instrument.price < 2000);
+                return (instrument.price > 1500 && instrument.price < 2000);
             });
         }
         if (item.checked && index == 4) {
